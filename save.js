083529c62cd0ba444f24465e59196894676c2217
gchfxb123 <240165@ucis.ac.th>
2026-02-05 18:31:23 +0700

    Create save.js

---
+++ save.js
@@ -0,0 +1,18 @@
+const Save = {
+  key: "NEON_MATCH3_SAVE",
+
+  save() {
+    localStorage.setItem(this.key, JSON.stringify({
+      coins: Currency.coins,
+      level: Levels.current
+    }));
+  },
+
+  load() {
+    const data = JSON.parse(localStorage.getItem(this.key));
+    if (!data) return;
+
+    Currency.coins = data.coins || 0;
+    Levels.current = data.level || 1;
+  }
+};
