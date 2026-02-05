812c40b922c2557ad0293eed32130de4f097f293
gchfxb123 <240165@ucis.ac.th>
2026-02-05 18:37:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-Save.load();
+AudioSystem.init();
+Board.init();
 UI.update();
-UI.updateCoins();
-UI.updateLevel();
-Tutorial.next();
+loop();
+
+document.getElementById("pauseBtn").onclick = () => {
+  STATE.paused = true;
+  UI.pause(true);
+};
+
+document.getElementById("resume").onclick = () => {
+  STATE.paused = false;
+  UI.pause(false);
+};
+
+document.getElementById("restart").onclick = () => {
+  STATE.score = 0;
+  STATE.combo = 1;
+  Board.init();
+  UI.pause(false);
+};
+AudioSystem.init();
+Board.init();
+UI.update();
+loop();
+
+document.getElementById("pauseBtn").onclick = () => {
+  STATE.paused = true;
+  UI.pause(true);
+};
+
+document.getElementById("resume").onclick = () => {
+  STATE.paused = false;
+  UI.pause(false);
+};
+
+document.getElementById("restart").onclick = () => {
+  STATE.score = 0;
+  STATE.combo = 1;
+  Board.init();
+  UI.pause(false);
+};
